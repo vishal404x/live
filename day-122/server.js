2862cd40d3845aff52da190735eff1b80b499e98
Raymond <vishal1.pdf@gmail.com>
2026-09-18 01:36:22 +0530

    downloaded the model

--- day-122/server.js
+++ day-122/server.js
@@ -1,11 +1,25 @@
 const express = require("express")
 const connectToDB = require("./src/db/db")
-
+const noteModel = require("./src/models/note.model")
 
 const app = express()
+app.use(express.json())
+
+app.post('/notes',async (req,res)=>{
+    const{title,content} =req.body
+    console.log(title,content);
+
+    await noteModel.create({
+        title,content
+    })
+    res.json({
+        message:"Note Created Successfully"
+    })
+    
+})
 
 
-connectToDB()
+// connectToDB()
 app.listen(3000,()=>{
     console.log("server is running on port 300");
     
--- day-122/src/db/db.js
+++ day-122/src/db/db.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
+const dns = require('dns')
+
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+])
 
 function connectToDB() {
-    mongoose.connect("mongodb+srv://magnesiamania_db_user:ToYfpAogVHYXuK9T@cluster0.bzktxhn.mongodb.net/cohort")
+    mongoose.connect("mongodb+srv://magnesiamania_db_user:ToYfpAogVHYXuK9T@cluster0.bzktxhn.mongodb.net/this")
     
     .then(()=>{
         console.log("Connected to DB");
